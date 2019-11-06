070b99bd7de26838d92adb032ec3d264e4bb9b56
Michael Fienen <fienen@gmail.com>
2019-11-06 11:15:13 -0600

    Reset the install

--- fractal.js
+++
@@ -1,5 +0,0 @@
-var fractal = require('@frctl/fractal').create();
-fractal.set('project.title', 'FooCorp Component Library');
-fractal.components.set('path', __dirname + '/src/components');
-
-module.exports = fractal;
