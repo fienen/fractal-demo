ab79daf6cf1c657f6ff553e907e8674f29e9be59
Michael Fienen <fienen@gmail.com>
2019-11-06 16:59:15 -0600

    Tweak default configuration

--- fractal.js
+++ fractal.js
@@ -11,9 +11,22 @@ const path = require('path');
 const fractal = module.exports = require('@frctl/fractal').create();
 
 /*
+ * Theme settings
+ */
+const mandelbrot = require('@frctl/mandelbrot');
+const myCustomisedTheme = mandelbrot({
+    panels: ["html", "view", "context", "resources", "info", "notes"],
+    skin: "grey"
+});
+
+fractal.web.theme(myCustomisedTheme);
+
+/*
  * Give your project a title.
  */
-fractal.set('project.title', 'Fractal Demo');
+fractal.set('project.title', 'Aquent Fractal Demo');
+fractal.set('project.version', 'v0.1');
+fractal.set('project.author', 'Michael Fienen');
 
 /*
  * Tell Fractal where to look for components.
@@ -29,3 +42,14 @@ fractal.docs.set('path', path.join(__dirname, 'docs'));
  * Tell the Fractal web preview plugin where to look for static assets.
  */
 fractal.web.set('static.path', path.join(__dirname, 'public'));
+
+/*
+ * Set up BrowserSync
+ */
+fractal.web.set('server.sync', true);
+fractal.web.set('server.syncOptions', {
+    open: true,
+    browser: 'google chrome',
+    notify: true,
+    watch: true
+});
